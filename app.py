7a387021d3b03cd6cccd5172fca874e33e92ae5f
Georgios <gkitsikoudis@gmx.de>
2022-09-08 16:04:46 +0200

    Create own file for db_functions All db statements are now called through db_functions.py Add logging

--- app.py
+++ app.py
@@ -1,79 +1,106 @@
 import sqlite3
-from flask import Flask, render_template, request, redirect, send_file
-from csv import writer
+import db_functions as db
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    send_file
+)
 
 
 app = Flask(__name__)
-INSERT = 'INSERT INTO person (Name, Firstname, Age) VALUES (?, ?, ?)'
+DATABASE = 'database.db'
+TABLE = 'Person'
+INSERT = 'INSERT INTO Person (Name, Firstname, Age) VALUES (?, ?, ?)'
 DELETE = 'DELETE FROM person WHERE Id=?'
-
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
-def update_csv():
-    conn = get_db_connection()
-    data = conn.execute('SELECT * FROM person')
-    with open('./csv/output.csv', 'w', newline='') as f:
-        wr = writer(f, dialect='excel')
-        wr.writerow([header[0] for header in data.description])
-        wr.writerows(data)
-    conn.close()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        conn = get_db_connection()
-        data = conn.execute('SELECT * FROM person')
-        headings = [header[0] for header in data.description]
-        conn.close
+        data = db.exec(
+            app=app,
+            database=DATABASE,
+            query=f'SELECT * FROM {TABLE}'
+        )
+        descriptions = [desc[0] for desc in data.description]
 
         return render_template(
             'index.html',
-            headings=headings,
+            headings=descriptions,
             data=data,
             title='Table'
         )
     elif request.method == 'POST':
         id = request.form['delete']
-        conn = get_db_connection()
-        conn.execute(DELETE, id)
-        conn.commit()
-        conn.close()
-        update_csv()
+        db.delete_by_id_from_table(
+            app=app,
+            database=DATABASE,
+            table=TABLE,
+            id=id
+        )
+        db.db_table_to_csv(
+            app=app,
+            database=DATABASE,
+            table=TABLE,
+            path=f"./csv/{DATABASE}_{TABLE}.csv"
+        )
 
         return redirect('/')
 
 
 @app.route('/create/', methods=['GET', 'POST'])
 def create():
+    data = db.exec(
+        app=app,
+        database=DATABASE,
+        query=f'SELECT * FROM {TABLE}',
+    )
+    descriptions = [desc[0] for desc in data.description]
     if request.method == 'GET':
-        conn = get_db_connection()
-        data = conn.execute('SELECT * FROM person')
-        headers = [header[0] for header in data.description]
         return render_template(
             'create.html',
-            inputs=headers,
+            inputs=descriptions,
             title='Table'
         )
     elif request.method == 'POST':
         if request.form['submit'] == 'add_person':
             try:
-                name = request.form['name']
-                firstname = request.form['firstname']
-                age = request.form['age']
-                conn = get_db_connection()
-                conn.execute(INSERT, (name, firstname, int(age)))
-                conn.commit()
-                conn.close()
-                update_csv()
+                pragma = db.get_pragma_info_of_table(
+                    app=app,
+                    database=DATABASE,
+                    table=TABLE
+                )
+                db.insert_into_table(
+                    app=app,
+                    database=DATABASE,
+                    table=TABLE,
+                    kwargs={
+                        title: db.type_casting(
+                            app=app,
+                            type=pragma[title],
+                            value=request.form[title]
+                        )
+                        for title in pragma.keys()
+                        if request.form[title]
+                    }
+                )
+                db.db_table_to_csv(
+                    app=app,
+                    database=DATABASE,
+                    table=TABLE,
+                    path=f"./csv/{DATABASE}_{TABLE}.csv"
+                )
 
                 return redirect('/')
-            except (TypeError, KeyError, sqlite3.DatabaseError):
+            except (
+                TypeError,
+                KeyError,
+                sqlite3.DatabaseError,
+                ValueError
+            ) as e:
+                app.logger.error(e)
                 return redirect('/create/')
 
 
@@ -82,5 +109,10 @@ def get_csv():
     return send_file('./csv/output.csv')
 
 
+def main(database: str, table_to_display: str):
+    pass
+
+
 if __name__ == '__main__':
+    # main()
     app.run(use_reloader=True, debug=True)
---
+++ db_functions.py
@@ -0,0 +1,103 @@
+import sqlite3
+from csv import writer
+
+
+def get_connection(database: str):
+    conn = sqlite3.connect(database)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def exec(app, database: str, query: str, **parameters):
+    try:
+        with get_connection(database) as conn:
+            result = conn.execute(query, parameters)
+        app.logger.info(f'"{query}" was executed')
+        return result
+    except sqlite3.DatabaseError as e:
+        app.logger.error(e)
+        raise e
+
+
+def exec_script(app, database: str, path_to_script: str):
+    try:
+        with get_connection(database) as conn:
+            with open(path_to_script, 'r') as f:
+                result = conn.executescript(f.read())
+        app.logger.info(f'Script at "{path_to_script}" excecuted')
+        return result
+    except (FileNotFoundError, sqlite3.DatabaseError) as e:
+        app.logger.error(e)
+        raise e
+
+
+def insert_into_table(app, database: str, table: str, kwargs):
+    try:
+        print(kwargs)
+        with get_connection(database) as conn:
+            k_string = ', '.join(kwargs.keys())
+            v_string = ', '.join(kwargs.values())
+            conn.execute(
+                f'INSERT INTO {table} ({k_string})\nVALUES({v_string});'
+            )
+        app.logger.info(f'{kwargs} was inserted into "{database}/{table}"')
+    except (sqlite3.DatabaseError, ValueError) as e:
+        app.logger.error(e)
+        raise e
+
+
+def delete_by_id_from_table(app, database: str, table: str, id):
+    try:
+        with get_connection(database) as conn:
+            conn.execute(f'DELETE FROM {table} WHERE Id=?', id)
+        app.logger.info(
+            f'Row with Id={id} was deleted from "{database}/{table}"'
+        )
+    except sqlite3.DatabaseError as e:
+        app.logger.error(e)
+        raise e
+
+
+def get_pragma_info_of_table(app, database: str, table: str):
+    try:
+        with get_connection(database) as conn:
+            pragma = conn.execute(
+                f'SELECT name, type FROM pragma_table_info("{table}")'
+            ).fetchall()
+            return {
+                tup[0]: tup[1]
+                for tup in pragma
+            }
+    except sqlite3.DatabaseError as e:
+        app.logger.error(e)
+        raise e
+
+
+def db_table_to_csv(app, database: str, table: str, path: str):
+    try:
+        with get_connection(database) as conn:
+            data = conn.execute(f'SELECT * FROM {table}')
+            with open(path, 'w', newline='') as f:
+                wr = writer(f, dialect='excel')
+                wr.writerow([header[0] for header in data.description])
+                wr.writerows(data)
+        app.logger.info(
+            f'CSV of "{database}/{table}" was created'
+        )
+    except (FileNotFoundError, sqlite3.DatabaseError) as e:
+        app.logger.error(e)
+        raise e
+
+
+def type_casting(app, type: str, value):
+    TYPE_CASTING = {
+        'TEXT': str,
+        'INTEGER': int,
+        'REAL': float
+    }
+    try:
+        casted_val = TYPE_CASTING.get(type)(value)
+        return casted_val
+    except (TypeError, KeyError) as e:
+        app.logger.error(e)
+        raise e
