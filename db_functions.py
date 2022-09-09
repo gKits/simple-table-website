d8af38c1a22ba730e9866428c1501f5bb21a3a03
Georgios <gkitsikoudis@gmx.de>
2022-09-09 09:52:31 +0200

    Fix db_functions.py Results are now returned as lists instead of db_cursors to close running db operation

--- db_functions.py
+++ db_functions.py
@@ -3,7 +3,7 @@ from csv import writer
 
 
 def get_connection(database: str):
-    conn = sqlite3.connect(database)
+    conn = sqlite3.connect(database, timeout=10)
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -11,7 +11,7 @@ def get_connection(database: str):
 def exec(app, database: str, query: str, **parameters):
     try:
         with get_connection(database) as conn:
-            result = conn.execute(query, parameters)
+            result = conn.execute(query, parameters).fetchall()
         app.logger.info(f'"{query}" was executed')
         return result
     except sqlite3.DatabaseError as e:
@@ -23,7 +23,7 @@ def exec_script(app, database: str, path_to_script: str):
     try:
         with get_connection(database) as conn:
             with open(path_to_script, 'r') as f:
-                result = conn.executescript(f.read())
+                result = conn.executescript(f.read()).fetchall()
         app.logger.info(f'Script at "{path_to_script}" excecuted')
         return result
     except (FileNotFoundError, sqlite3.DatabaseError) as e:
@@ -31,16 +31,16 @@ def exec_script(app, database: str, path_to_script: str):
         raise e
 
 
-def insert_into_table(app, database: str, table: str, kwargs):
+def insert_into_table(app, database: str, table: str, to_insert):
     try:
-        print(kwargs)
         with get_connection(database) as conn:
-            k_string = ', '.join(kwargs.keys())
-            v_string = ', '.join(kwargs.values())
+            k_string = ', '.join(to_insert.keys())
+            v_string = ', '.join(['?' for _ in to_insert.keys()])
             conn.execute(
-                f'INSERT INTO {table} ({k_string})\nVALUES({v_string});'
+                f'INSERT INTO {table} ({k_string}) VALUES({v_string});',
+                [val for val in to_insert.values()]
             )
-        app.logger.info(f'{kwargs} was inserted into "{database}/{table}"')
+        app.logger.info(f'{to_insert} was inserted into "{database}/{table}"')
     except (sqlite3.DatabaseError, ValueError) as e:
         app.logger.error(e)
         raise e
@@ -64,10 +64,7 @@ def get_pragma_info_of_table(app, database: str, table: str):
             pragma = conn.execute(
                 f'SELECT name, type FROM pragma_table_info("{table}")'
             ).fetchall()
-            return {
-                tup[0]: tup[1]
-                for tup in pragma
-            }
+        return {tup[0]: tup[1] for tup in pragma}
     except sqlite3.DatabaseError as e:
         app.logger.error(e)
         raise e
@@ -89,7 +86,7 @@ def db_table_to_csv(app, database: str, table: str, path: str):
         raise e
 
 
-def type_casting(app, type: str, value):
+def type_casting(type: str, value):
     TYPE_CASTING = {
         'TEXT': str,
         'INTEGER': int,
@@ -99,5 +96,4 @@ def type_casting(app, type: str, value):
         casted_val = TYPE_CASTING.get(type)(value)
         return casted_val
     except (TypeError, KeyError) as e:
-        app.logger.error(e)
         raise e
