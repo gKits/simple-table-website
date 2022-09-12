2ae01c25ef3c4d97bcc6fd080d330bc6a1791283
Georgios <gkitsikoudis@gmx.de>
2022-09-12 15:46:50 +0200

    Update db_function Add docstrings Split pragma function into individual functions get_row_names get_row_types get_row_default_values get_row_not_null_status

--- db_functions.py
+++ db_functions.py
@@ -2,22 +2,42 @@ import sqlite3
 from csv import writer
 
 
-def get_connection(database: str):
+def get_connection(database: str) -> sqlite3.Connection:
+    '''
+    This function will create a database connection and return it
+
+    :param: database: The name of the database (.db File)
+    :return: The database connection
+    '''
     conn = sqlite3.connect(database, timeout=10)
     conn.row_factory = sqlite3.Row
     return conn
 
 
-def exec(database: str, query: str, **parameters):
+def exec(database: str, command: str):
+    '''
+    This function executes a given command on the database
+
+    :param: database: The name of the database (.db File)
+    :param: command: The command to execute
+    :return: Cursor result as list
+    '''
     try:
         with get_connection(database) as conn:
-            result = conn.execute(query, parameters).fetchall()
+            result = conn.execute(command).fetchall()
         return result
     except sqlite3.DatabaseError as e:
         raise e
 
 
 def exec_script(database: str, path_to_script: str):
+    '''
+    This function executes a given script on the database
+
+    :param: database: The name of the database (.db File)
+    :param: path_to_script: The directory path to the script
+    :return: Cursor result as list
+    '''
     try:
         with get_connection(database) as conn:
             with open(path_to_script, 'r') as f:
@@ -27,7 +47,15 @@ def exec_script(database: str, path_to_script: str):
         raise e
 
 
-def insert_into_table(database: str, table: str, to_insert):
+def insert_into_table(database: str, table: str, **to_insert):
+    '''
+    This function will perform an INSERT statement into a given table
+
+    :param: database: The name of the database (.db File)
+    :param: table: The name of the table to insert into
+    :param: *parameters: Parameters to replace "?" characters in command
+    :return: Cursor result as list
+    '''
     try:
         with get_connection(database) as conn:
             k_string = ', '.join(to_insert.keys())
@@ -48,13 +76,75 @@ def delete_by_id_from_table(database: str, table: str, id):
         raise e
 
 
-def get_pragma_info_of_table(database: str, table: str):
+def get_row_names(database: str, table: str) -> list:
+    '''
+    This function will return all row names of a given table
+
+    :param: database: The name of the database (.db File)
+    :param: table: The name of the table to get the row names from
+    :return: list with all row names
+    '''
+    try:
+        with get_connection(database) as conn:
+            pragma = conn.execute(
+                f'SELECT name FROM pragma_table_info("{table}")'
+            ).fetchall()
+        return [row[0] for row in pragma]
+    except sqlite3.DatabaseError as e:
+        raise e
+
+
+def get_row_types(database: str, table: str) -> dict:
+    '''
+    This function will return the types of all rows of a given table
+
+    :param: database: The name of the database (.db File)
+    :param: table: The name of the table to get the row types from
+    :return: dict with the row names as keys and types as values
+    '''
     try:
         with get_connection(database) as conn:
             pragma = conn.execute(
                 f'SELECT name, type FROM pragma_table_info("{table}")'
             ).fetchall()
-        return {tup[0]: tup[1] for tup in pragma}
+        return {row[0]: row[1] for row in pragma}
+    except sqlite3.DatabaseError as e:
+        raise e
+
+
+def get_row_default_values(database: str, table: str) -> dict:
+    '''
+    This function will return the defaul values of all rows of a given table
+
+    :param: database: The name of the database (.db File)
+    :param: table: The name of the table to get the default values from
+    :return: dict with the row names as keys and types as values
+    '''
+    try:
+        with get_connection(database) as conn:
+            pragma = conn.execute(
+                f'SELECT name, dflt_value FROM pragma_table_info("{table}")'
+            ).fetchall()
+        return {row[0]: row[1] for row in pragma}
+    except sqlite3.DatabaseError as e:
+        raise e
+
+
+def get_row_not_null_status(database: str, table: str) -> dict:
+    '''
+    This function will return the value of the not null constraint of all row
+    of a given table
+
+    :param: database: The name of the database (.db File)
+    :param: table: The name of the table to get the default values from
+    :return: dict with the row names as keys and types as values
+    '''
+    try:
+        with get_connection(database) as conn:
+            pragma = conn.execute(
+                f'SELECT name, notnull FROM pragma_table_info("{table}")'
+            ).fetchall()
+        return {row[0]: row[1] for row in pragma}
     except sqlite3.DatabaseError as e:
         raise e
 
