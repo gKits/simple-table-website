1c9705c0f370e373c0871857929e76f61e4dbe48
gKits <gkitsikoudis@gmx.de>
2022-09-26 20:39:21 +0200

    Rewrite argparsing

--- tablewebsite.py
+++ tablewebsite.py
@@ -1,54 +1,54 @@
 import argparse
+import sys
+from os import path
 from app import run
+from provision_db import provision_db
 
 
-def main_no():
-    parser = argparse.ArgumentParser(
-        'simple-table-website',
-        description='Website arguments'
-    )
-    parser.add_argument(
-        'database',
-        type=str,
-        help='The name of the database file containing the data.'
-    )
-    parser.add_argument(
-        'table',
-        type=str,
-        help='The name of the table that you want to display.'
-    )
-    parser.add_argument(
-        '-p', '--port',
-        type=int,
-        action='store',
-        nargs='?',
-        default=5000,
-        help='''The port you want to run the application on.
-             Default value = 5000'''
-    )
+def parse_args():
+    parser = argparse.ArgumentParser('Table website', description='A small and interactive Flask website displaying the data of a SQLite DB')
 
+    # Sub parsers
+    sub_parser = parser.add_subparsers(required=True)
+    parser_run = sub_parser.add_parser('run', help='Run your webiste')
+    parser_db = sub_parser.add_parser('database', help='Function for provisioning your SQLite DB')
+
+    # Run parser arguments
+    parser_run.add_argument('database', type=str, metavar='/path/to/database.db', help='.db file runnning your database')
+    parser_run.add_argument('table', type=str, help='Name of the table you want to display')
+    parser_run.add_argument('-n', '--name', type=str, nargs='?', default='tableapp', help='Specify your websites name')
+    parser_run.add_argument('-p', '--port', type=int, nargs='?', default=5000, help='Port you want to run the website on')
+
+    # DB parser arguments
+    group_db_sql = parser_db.add_mutually_exclusive_group()
+    parser_db.add_argument('-d', '--database', type=str, metavar='/path/to/database.db', default='./db/database.db', help='.db file runnning your database')
+    group_db_sql.add_argument('-s', '--sql', type=str, nargs='+', metavar='/path/to/scheme.sql', help='Use specific .sql files to run on DB')
+    group_db_sql.add_argument('-S', '--sqldir', type=str, nargs='?', metavar='/dir/with/sqls/database-tablename.csv', default='./sql/', help='Use a specific directory containing .sql files to run on DB')
+    parser_db.add_argument('-i', '--insert', type=str, nargs='+', metavar='tablename /path/to/table.csv', help='Tuple of tablename and csv table to insert into DB')
+    parser_db.add_argument('-r', '--reprovision', action='store_true', help='If set the existing DB will be deleted and reprovisioned')
+
+    # Parse arguments
     args = vars(parser.parse_args())
-    run(database=args['database'], table=args['table'], port=args['port'])
+
+    used_sub_parser = sys.argv[1]
+
+    if used_sub_parser == 'database':
+        if args['insert'] and len(args['insert']) % 2 == 0:
+            args['insert'] = [(args['insert'][i], args['insert'][i+1]) for i in range(0, len(args['insert']), 2)]
+        else:
+            raise 'error: argument -i/--insert needs a tablename and path foreach csv file'
+
+    return used_sub_parser, args
 
 
 def main():
-    parser = argparse.ArgumentParser('Table website', description='A small and interactive Flask website displaying the data of a SQLite DB')
+    used_sub_parser, args = parse_args()
 
-    sub_parser = parser.add_subparsers(help='sub help')
+    if used_sub_parser == 'database':
+        provision_db()
+    elif used_sub_parser == 'run':
+        run()
 
-    # Run parser
-    parser_run = sub_parser.add_parser('run', help='Run your webiste')
-    parser_run.add_argument('database', type=str, metavar='/path/to/database.db', help='Directory path to your .db file')
-    parser_run.add_argument('table', type=str, help='Name of the table you want to display')
-    parser_run.add_argument('-p', '--port', type=int, nargs='?', help='Port you want to run the website on')
-
-    # DB parser
-    parser_db = sub_parser.add_parser('database', help='Function for provisioning your SQLite DB')
-    # parser_db.add_argument()
-    parser_db.add_argument('-r', '--reprovision', action='store_true', help='If set the DB will be deleted and reprovisioned')
-
-    args = vars(parser.parse_args())
-    print(args)
 
 if __name__ == '__main__':
     main()
