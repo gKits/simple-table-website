82332e26982cc450eb185220e0f1652e64563d0c
Georgios Kitsikoudis <georgios.kitsikoudis@telekom.de>
2022-10-13 11:14:46 +0200

    Adjust parser arguments

--- tablewebsite.py
+++ tablewebsite.py
@@ -10,21 +10,21 @@ def parse_args():
     # Sub parsers
     sub_parser = parser.add_subparsers(required=True)
     parser_run = sub_parser.add_parser('run', help='Run your webiste')
-    parser_db = sub_parser.add_parser('database', help='Function for provisioning your SQLite DB')
+    parser_db = sub_parser.add_parser('database', help='Functions for provisioning your SQLite DB')
 
     # Run parser arguments
     parser_run.add_argument('table', type=str, help='Name of the table you want to display')
-    parser_run.add_argument('-d', '--database', type=str, metavar='/path/to/database.db', default='./db/database.db', help='Path to .db file runnning your database')
-    parser_run.add_argument('-n', '--name', type=str, nargs='?', default='tableapp', help='Name your websites')
-    parser_run.add_argument('-p', '--port', type=int, nargs='?', default=5000, help='Specify the port you want to run the website on')
+    parser_run.add_argument('-d', '--database', type=str, metavar='/path/to/database.db', default='./db/database.db', help='Path to .db file.')
+    parser_run.add_argument('-n', '--name', type=str, nargs='?', default='tableapp', help='Desired name of website.')
+    parser_run.add_argument('-p', '--port', type=int, nargs='?', default=5000, help='Port to run website on.')
+    parser_run.add_argument('-b', '--autobackup', action='store_true', help='Auto backup the DB tables in .csv files. Set directory with -B / --backupdir.')
+    parser_run.add_argument('-B', '--backupdir', type=str, nargs='?', default='./csv', help='.csv files of DB tables will be saved in dir. To autoback use -b / --autobackup.')
 
     # DB parser arguments
-    group_db_sql = parser_db.add_mutually_exclusive_group()
-    parser_db.add_argument('-d', '--database', type=str, metavar='/path/to/database.db', default='./db/database.db', help='Path to .db file runnning your database')
-    group_db_sql.add_argument('-s', '--sql', type=str, nargs='+', metavar='/path/to/scheme.sql', help='Use specific .sql files to run on DB')
-    group_db_sql.add_argument('-S', '--sqldir', type=str, nargs='?', metavar='/dir/with/sqls/database-tablename.csv', default='./sql/', help='Use a specific directory containing .sql files to run on DB')
-    parser_db.add_argument('-i', '--insert', type=str, nargs='+', metavar='tablename /path/to/table.csv', help='Tuple of tablename and csv table to insert into DB')
-    parser_db.add_argument('-r', '--reprovision', action='store_true', help='If set the existing DB will be deleted and reprovisioned')
+    parser_db.add_argument('-d', '--database', type=str, metavar='/path/to/database.db', default='./db/database.db', help='Path to .db file.')
+    parser_db.add_argument('-s', '--sql', type=str, nargs='?', metavar='/path/to/scheme.sql', default='./sql/schema.sql', help='.sql file to execute on DB creation.')
+    parser_db.add_argument('-i', '--insert', type=str, nargs='+', metavar='tablename /path/to/table.csv', help='Tuples of tablenames and csv tables to insert into DB')
+    parser_db.add_argument('-r', '--reprovision', action='store_true', help='Existing DB will be deleted and reprovisioned')
 
     # Parse arguments
     args = vars(parser.parse_args())
@@ -44,7 +44,7 @@ def main():
     used_sub_parser, args = parse_args()
 
     if used_sub_parser == 'database':
-        provision_db(db_path=args['database'], sql_dir=args["sqldir"], sql_paths=args['sql'], insert_tables=args['insert'], reprovision=args['reprovision'])
+        provision_db(db_path=args['database'], sql_dir=args["sqldir"], sql_path=args['sql'], insert_tables=args['insert'], reprovision=args['reprovision'])
     elif used_sub_parser == 'run':
         run(db_path=args['database'], table=args['table'], name=args['name'], port=args['port'])
 
